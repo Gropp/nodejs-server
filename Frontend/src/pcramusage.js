defcedc34389037707a1d3f185a6aeda05f348f2
Gropp <fgropp@gmail.com>
2022-08-17 16:20:51 -0300

    nodejs-server

--- Frontend/src/pcramusage.js
+++ Frontend/src/pcramusage.js
@@ -3,20 +3,33 @@ const os = require('node:os');
 //retorna atributos, propriedades e funcoes do objeto OS - sistema operacional
 //console.log(os)
 
-//vamos descontruir o objeto OS - separando somente os metodos de interesse
-const {arch, platform, totalmem, freemem} = os;
+//vamos criar um ciclo de repeticao - 1000ms
+//vai executar essa funcao a cada 1 segundo
+setInterval(() => {
 
-//vamos tratar as variaveis de memoria para que elas mostrem em Gb e nao em bytes
-const tRAM = totalmem()/1024/1024;
-const fRAM = freemem()/1024/1024;
+    //vamos descontruir o objeto OS - separando somente os metodos de interesse
+    const {arch, platform, totalmem, freemem} = os;
 
-//depois de desconstruir, criamos um objeto que vai receber esses metodos selecionados
-const stats  = {
-    OS: platform(),
-    Arch: arch(),
-    TotalRAM: parseInt(tRAM),
-    FreeRAM: parseInt(fRAM),
-}
+    //vamos tratar as variaveis de memoria para que elas mostrem em Gb e nao em bytes
+    const tRAM = totalmem()/1024/1024;
+    const fRAM = freemem()/1024/1024;
+    const usage = (fRAM/tRAM)* 100
 
-//testamos mandando imprirmir o objeto criado
-console.log(stats)
+    //depois de desconstruir, criamos um objeto que vai receber esses metodos selecionados
+    const stats  = {
+        OS: platform(),
+        Arch: arch(),
+        TotalRAM: `${parseInt(tRAM)} MB`,
+        FreeRAM: `${parseInt(fRAM)} MB`,
+        Usage: `${usage.toFixed(2)} %`,
+    }
+    //testamos mandando imprirmir o objeto criado
+    //limpa o console
+    console.clear()
+    //console.log(stats)
+    console.table(stats)
+
+    //exportando o objeto stats para fora deste componente
+    exports.stats = stats
+},1000);
+
--- server.js
+++ server.js
@@ -6,9 +6,20 @@ const port = 3000;
 //o conteudo da constante http criada, é o conteudo do comando http dentro do node, tem todas as propriedades, metodos e informacoes sobre esse protocolo
 //console.log(http)
 
+//importando o modulo criado para "rodar no servidor"
+//como queremos que a funcao execute aqui se usa a palavra reservada require
+const stats = require('./Frontend/src/pcramusage.js');
+
 //criamos um servidor e mandamos ele escutar a porta 3000
 //ao criar o servidor vamos ter uma requisicao, uma resposta e uma funcao
 //para efetuar uma requisicao/resposta e parar, usamos o res.end
 http.createServer((req, res) => {
-    res.end('<h1>Working</h1>')
+    let url = req.url;
+
+    if(url === "/") {
+        res.end('<h1>Working Server - HOME</h1>')
+    }else if(url === "/stats") {
+        res.end(JSON.stringify(stats, null, 2));
+    }
+    
 }).listen(port, () => console.log(`server is running in ${host}:${port}`));
